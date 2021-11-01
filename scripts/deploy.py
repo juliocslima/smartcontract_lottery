4cbda07cd494d9700d0d6467b07cee448bd2c2bb
juliocslima <lima.julio.cs@gmail.com>
2021-11-01 11:20:17 -0300

    Enter lottery function created

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -3,7 +3,7 @@ from scripts.helpful_scripts import get_account, get_contract
 
 
 def deploy_lottery():
-    account = get_account(id="freecodecamp-account")
+    account = get_account()
     lottery = Lottery.deploy(
         get_contract("eth_usd_price_feed").address,
         get_contract("vrf_coordinator").address,
@@ -17,13 +17,23 @@ def deploy_lottery():
 
 
 def start_lottery():
-    account = get_account(id="freecodecamp-account")
+    account = get_account()
     lottery = Lottery[-1]
     starting_tx = lottery.startLottery({"from": account})
     starting_tx.wait(1)
     print("The lottery is started!")
 
 
+def enter_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    value = lottery.getEntranceFee() + 100000000
+    tx = lottery.enter({"from": account, "value": value})
+    tx.wait(1)
+    print("You entered the lottery!")
+
+
 def main():
     deploy_lottery()
     start_lottery()
+    enter_lottery()
