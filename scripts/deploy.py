b403b058676a867450852739133bde4fb63a3951
juliocslima <lima.julio.cs@gmail.com>
2021-11-01 11:09:07 -0300

    Deploying mock contracts and helpful_scripts

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,14 +1,19 @@
-from brownie import Lottery, network, config, accounts
-from web3 import Web3
+from brownie import Lottery, network, config
+from scripts.helpful_scripts import get_account, get_contract
 
 
 def deploy_lottery():
-    account = accounts[0]
+    account = get_account(id="freecodecamp-account")
     lottery = Lottery.deploy(
-        config["networks"][network.show_active()]["eth_usd_price_feed"],
+        get_contract("eth_usd_price_feed").address,
+        get_contract("vrf_coordinator").address,
+        get_contract("link_token").address,
+        config["networks"][network.show_active()]["fee"],
+        config["networks"][network.show_active()]["keyhash"],
         {"from": account},
+        publish_source=config["networks"][network.show_active()].get(
+            "verify", False),
     )
-    print(lottery.getLatestPrice())
 
 
 def main():
